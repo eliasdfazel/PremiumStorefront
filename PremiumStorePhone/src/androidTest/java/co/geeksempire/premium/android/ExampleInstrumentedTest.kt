3f423cd64c2eaf8d4e176ba930b62c8f7222edd8
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-04-12 08:49:32 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- PremiumStorePhone/src/androidTest/java/co/geeksempire/premium/android/ExampleInstrumentedTest.kt
+++ PremiumStorePhone/src/androidTest/java/co/geeksempire/premium/android/ExampleInstrumentedTest.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/12/21 8:49 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 package co.geeksempire.premium.android
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
