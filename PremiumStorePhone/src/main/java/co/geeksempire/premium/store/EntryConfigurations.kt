744af05af79e48dd3a3926f8863a541fde50a5da
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-04-13 09:36:42 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- PremiumStorePhone/src/main/java/co/geeksempire/premium/store/EntryConfigurations.kt
+++ PremiumStorePhone/src/main/java/co/geeksempire/premium/store/EntryConfigurations.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 4/12/21 11:16 AM
+ * Last modified 4/13/21 9:32 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -10,8 +10,11 @@
 
 package co.geeksempire.premium.store
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.premium.store.StorefrontConfigurations.UserInterface.Storefront
 import co.geeksempire.premium.store.databinding.EntryConfigurationsLayoutBinding
 
 class EntryConfigurations : AppCompatActivity() {
@@ -22,6 +25,12 @@ class EntryConfigurations : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         entryConfigurationsLayoutBinding = EntryConfigurationsLayoutBinding.inflate(layoutInflater)
         setContentView(entryConfigurationsLayoutBinding.root)
+
+        startActivity(Intent().apply {
+            setClass(applicationContext, Storefront::class.java)
+        }, ActivityOptions.makeCustomAnimation(applicationContext, R.anim.fade_in, 0).toBundle())
+
+        this@EntryConfigurations.finish()
 
     }
 
