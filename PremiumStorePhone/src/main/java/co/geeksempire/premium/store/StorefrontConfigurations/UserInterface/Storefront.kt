023ec99dc918c1e8624ba2da36c053319bfe6fdf
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-04-13 08:31:58 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ PremiumStorePhone/src/main/java/co/geeksempire/premium/store/StorefrontConfigurations/UserInterface/Storefront.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/13/21 8:31 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.premium.store.StorefrontConfigurations.UserInterface
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Storefront : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+}
