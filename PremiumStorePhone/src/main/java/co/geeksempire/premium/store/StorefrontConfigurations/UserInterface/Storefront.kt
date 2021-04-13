47f2b1746e1d36eaf366dc7327b25fa354a4f9c2
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-04-13 09:22:29 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- PremiumStorePhone/src/main/java/co/geeksempire/premium/store/StorefrontConfigurations/UserInterface/Storefront.kt
+++ PremiumStorePhone/src/main/java/co/geeksempire/premium/store/StorefrontConfigurations/UserInterface/Storefront.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 4/13/21 8:31 AM
+ * Last modified 4/13/21 8:40 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,11 +12,16 @@ package co.geeksempire.premium.store.StorefrontConfigurations.UserInterface
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.premium.store.databinding.StorefrontLayoutBinding
 
 class Storefront : AppCompatActivity() {
 
+    lateinit var storefrontLayoutBinding: StorefrontLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        storefrontLayoutBinding = StorefrontLayoutBinding.inflate(layoutInflater)
+        setContentView(storefrontLayoutBinding.root)
 
 
 
