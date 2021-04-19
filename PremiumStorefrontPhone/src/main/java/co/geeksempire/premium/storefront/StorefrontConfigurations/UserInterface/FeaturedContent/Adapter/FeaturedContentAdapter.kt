45e22a7b0eb51a264eea5452775cce3f05c74978
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-04-18 16:10:38 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/UserInterface/FeaturedContent/Adapter/FeaturedContentAdapter.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/18/21 4:10 PM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.premium.storefront.StorefrontConfigurations.UserInterface.FeaturedContent.Adapter
+
+class FeaturedContentAdapter {
+}
---
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/UserInterface/FeaturedContent/ViewHolder/FeaturedContentViewHolder.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 4/18/21 4:10 PM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.premium.storefront.StorefrontConfigurations.UserInterface.FeaturedContent.ViewHolder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class FeaturedContentViewHolder (rootItemView: View) : RecyclerView.ViewHolder(rootItemView) {
+
+}
