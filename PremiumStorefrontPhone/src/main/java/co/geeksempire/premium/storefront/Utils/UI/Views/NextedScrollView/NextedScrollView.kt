d142a248280ae3a3c501330f93696fb4d79eb2df
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-05-17 00:49:11 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/Utils/UI/Views/NextedScrollView/NextedScrollView.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/Utils/UI/Views/NextedScrollView/NextedScrollView.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 5/17/21, 12:37 AM
+ * Last modified 5/17/21, 12:49 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,37 +11,38 @@
 package co.geeksempire.premium.storefront.Utils.UI.Views.NextedScrollView
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.util.AttributeSet
 import androidx.core.widget.NestedScrollView
-import co.geeksempire.premium.storefront.R
+import net.geeksempire.balloon.optionsmenu.library.Utils.dpToInteger
 
 
 class NextedScrollView(context: Context, attributesSet: AttributeSet) : NestedScrollView(context, attributesSet) {
 
-    var attributeSet: TypedArray = context.obtainStyledAttributes(attributesSet, R.styleable.NextedScrollView)
+    private var scrollAmount = 0
 
-    val isScrollable: Boolean = true
-
-    var scrollAmount = 0
+    private var distanceToScroll = dpToInteger(context, 199) * 3
 
     override fun fling(velocityY: Int) {
-        super.fling(velocityY/3)
 
-//        if (velocityY < 0) {
-//
-//            smoothScrollTo(scrollY, scrollAmount - 531, 1579)
-//
-//            scrollAmount -= 531
-//
-//        } else {
-//
-//            smoothScrollTo(scrollY, scrollAmount + 531, 1579)
-//
-//            scrollAmount += 531
-//
-//        }
+        if (velocityY < 0) {
 
+            smoothScrollTo(scrollY, scrollAmount - distanceToScroll, 1777)
+
+            scrollAmount -= distanceToScroll
+
+        } else {
+
+            smoothScrollTo(scrollY, scrollAmount + distanceToScroll, 1777)
+
+            scrollAmount += distanceToScroll
+
+        }
+
+    }
+
+    fun distanceToScroll(newDistanceToScroll: Int) {
+
+        distanceToScroll = newDistanceToScroll
     }
 
 }
