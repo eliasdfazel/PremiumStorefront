1dc85e1f41f545e0d05a55e031f66950a4591875
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-05-30 13:47:10 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/FavoriteProductsConfigurations/UserInterface/FavoriteProducts.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/FavoriteProductsConfigurations/UserInterface/FavoriteProducts.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 5/30/21, 1:04 PM
+ * Last modified 5/30/21, 1:46 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,10 +12,20 @@ package co.geeksempire.premium.storefront.FavoriteProductsConfigurations.UserInt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.premium.storefront.R
 
 class FavoriteProducts : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    override fun onBackPressed() {
+
+        this@FavoriteProducts.finish()
+
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_from_left)
+
+    }
+
 }
