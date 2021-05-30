f6f42b863d33a07014b6ed6bbe1eb3c7c0ba4a5e
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-05-30 13:55:20 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/FavoriteProductsConfigurations/UserInterface/FavoriteProducts.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/FavoriteProductsConfigurations/UserInterface/FavoriteProducts.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 5/30/21, 1:46 PM
+ * Last modified 5/30/21, 1:55 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -13,11 +13,18 @@ package co.geeksempire.premium.storefront.FavoriteProductsConfigurations.UserInt
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import co.geeksempire.premium.storefront.R
+import co.geeksempire.premium.storefront.databinding.FavoriteProductsLayoutBinding
 
 class FavoriteProducts : AppCompatActivity() {
 
+    lateinit var favoriteProductsLayoutBinding: FavoriteProductsLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        favoriteProductsLayoutBinding = FavoriteProductsLayoutBinding.inflate(layoutInflater)
+        setContentView(favoriteProductsLayoutBinding.root)
+
+
     }
 
     override fun onBackPressed() {
