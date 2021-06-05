480323f552d951e3f109002b1799d8f8f2bf2189
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-06-05 10:57:27 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/ContentFiltering/FilteringSetup.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/ContentFiltering/FilteringSetup.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 6/5/21, 8:40 AM
+ * Last modified 6/5/21, 10:55 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,11 +11,15 @@
 package co.geeksempire.premium.storefront.StorefrontConfigurations.Extensions
 
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.view.animation.OvershootInterpolator
 import androidx.recyclerview.widget.RecyclerView
+import co.geeksempire.premium.storefront.R
 import co.geeksempire.premium.storefront.StorefrontConfigurations.ContentFiltering.Filter.FilterOptionsItem
 import co.geeksempire.premium.storefront.StorefrontConfigurations.DataStructure.StorefrontFeaturedContentKey
 import co.geeksempire.premium.storefront.StorefrontConfigurations.UserInterface.Storefront
+import co.geeksempire.premium.storefront.Utils.UI.Animations.AnimationListener
+import co.geeksempire.premium.storefront.Utils.UI.Animations.CircularRevealAnimation
 import co.geeksempire.premium.storefront.Utils.UI.SmoothScrollers.RecycleViewSmoothLayoutList
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
@@ -24,13 +28,35 @@ import kotlin.math.absoluteValue
 fun Storefront.filteringSetup() {
 
     if (storefrontLayoutBinding.sortingInclude.root.isShown) {
+
+        storefrontLayoutBinding.sortingInclude.root.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.fade_out))
         storefrontLayoutBinding.sortingInclude.root.visibility = View.GONE
+
     }
 
-    storefrontLayoutBinding.filteringInclude.root.visibility = if (storefrontLayoutBinding.filteringInclude.root.isShown) {
-        View.GONE
+    if (storefrontLayoutBinding.filteringInclude.root.isShown) {
+
+        storefrontLayoutBinding.filteringInclude.root.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.fade_out))
+        storefrontLayoutBinding.filteringInclude.root.visibility = View.GONE
+
     } else {
-        View.VISIBLE
+
+        val animationListener = object : AnimationListener {
+
+            override fun animationFinished() {
+                super.animationFinished()
+
+
+
+            }
+
+        }
+
+        val circularRevealAnimation = CircularRevealAnimation (animationListener)
+        circularRevealAnimation.startForView(context = applicationContext, rootView = storefrontLayoutBinding.filteringInclude.root,
+            xPosition = ((storefrontLayoutBinding.rightActionView.x) + (storefrontLayoutBinding.rightActionView.width/2)).toInt(),
+            yPosition = ((storefrontLayoutBinding.rightActionView.y) + (storefrontLayoutBinding.rightActionView.height/2)).toInt())
+
     }
 
     val viewTranslateY = (storefrontLayoutBinding.filteringInclude.filterCountryView.y - storefrontLayoutBinding.filteringInclude.filterCompatibilitiesView.y).absoluteValue
--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/ContentSorting/SortingSetup.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/ContentSorting/SortingSetup.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 6/5/21, 10:07 AM
+ * Last modified 6/5/21, 10:57 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,20 +11,25 @@
 package co.geeksempire.premium.storefront.StorefrontConfigurations.Extensions
 
 import android.view.View
+import android.view.animation.AnimationUtils
+import co.geeksempire.premium.storefront.R
 import co.geeksempire.premium.storefront.StorefrontConfigurations.ContentFiltering.Filter.SortingOptions
 import co.geeksempire.premium.storefront.StorefrontConfigurations.UserInterface.Storefront
 
 fun Storefront.sortingSetup() {
 
     if (storefrontLayoutBinding.filteringInclude.root.isShown) {
+
+        storefrontLayoutBinding.filteringInclude.root.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.fade_out))
         storefrontLayoutBinding.filteringInclude.root.visibility = View.GONE
+
     }
 
-//    storefrontLayoutBinding.sortingInclude.root.visibility = if (storefrontLayoutBinding.sortingInclude.root.isShown) {
-//        View.GONE
-//    } else {
-//        View.VISIBLE
-//    }
+    if (storefrontLayoutBinding.sortingInclude.root.isShown) {
+        storefrontLayoutBinding.sortingInclude.root.visibility = View.GONE
+    } else {
+        storefrontLayoutBinding.sortingInclude.root.visibility = View.VISIBLE
+    }
 
     filterAllContent.sortAllContentByInput(allContentAdapter.storefrontContents, SortingOptions.SortByRating)
         .invokeOnCompletion {
