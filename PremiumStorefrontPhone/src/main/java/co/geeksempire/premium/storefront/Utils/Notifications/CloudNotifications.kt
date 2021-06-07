e154351cf71728baf14449a4b51e93ea865c177e
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-06-07 06:55:34 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/Utils/Notifications/CloudNotifications.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 6/7/21, 6:55 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.premium.storefront.Utils.Notifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class CloudNotifications : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+    }
+
+    override fun onNewToken(newToken: String) {
+        super.onNewToken(newToken)
+    }
+}
