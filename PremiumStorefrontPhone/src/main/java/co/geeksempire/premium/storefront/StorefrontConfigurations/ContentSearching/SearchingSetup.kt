d7fb81e963cbad01934e4218cd6a814a2b27e9a0
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-06-12 07:47:00 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/ContentSearching/SearchingSetup.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/ContentSearching/SearchingSetup.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 6/12/21, 7:43 AM
+ * Last modified 6/12/21, 7:45 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -64,6 +64,7 @@ fun Storefront.searchingSetup(themeType: Boolean = ThemeType.ThemeLight) {
 
                 storefrontLayoutBinding.textInputSearchView.layoutParams.width = dpToInteger(applicationContext, 51)
                 storefrontLayoutBinding.textInputSearchView.layoutParams.height = dpToInteger(applicationContext, 51)
+                storefrontLayoutBinding.textInputSearchView.requestLayout()
 
             }
 
