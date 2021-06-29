e2ad931c31771a81c0ea63f63b4420625a90a2ec
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-06-29 07:28:34 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/ProductsDetailsConfigurations/UserInterface/ProductDetailsFragment.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/ProductsDetailsConfigurations/UserInterface/ProductDetailsFragment.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 6/29/21, 7:24 AM
+ * Last modified 6/29/21, 7:28 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -316,7 +316,7 @@ class ProductDetailsFragment : Fragment() {
                                 startAnimation(AnimationUtils.loadAnimation(requireContext(), R.anim.slide_left_from_right_bounce))
                                 visibility = View.VISIBLE
 
-                            }, 333)
+                            }, 151)
 
                         }.setOnClickListener {
 
