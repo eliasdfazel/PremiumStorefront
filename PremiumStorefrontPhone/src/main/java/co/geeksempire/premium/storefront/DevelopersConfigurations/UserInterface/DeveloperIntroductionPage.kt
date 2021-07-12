03bef02a6bed2c5e347f5aa910add10c736813e2
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-07-12 13:17:04 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/DevelopersConfigurations/UserInterface/DeveloperIntroductionPage.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/DevelopersConfigurations/UserInterface/DeveloperIntroductionPage.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 7/12/21, 1:02 PM
+ * Last modified 7/12/21, 1:17 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,6 +12,7 @@ package co.geeksempire.premium.storefront.DevelopersConfigurations.UserInterface
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.geeksempire.premium.storefront.DevelopersConfigurations.DataStructure.DevelopersDataKey
 import co.geeksempire.premium.storefront.databinding.DeveloperIntroductionLayoutBinding
 
 class DeveloperIntroductionPage : AppCompatActivity() {
@@ -23,7 +24,35 @@ class DeveloperIntroductionPage : AppCompatActivity() {
         developerIntroductionLayoutBinding = DeveloperIntroductionLayoutBinding.inflate(layoutInflater)
         setContentView(developerIntroductionLayoutBinding.root)
 
+        if (intent != null) {
 
+            val developerName = intent.getStringExtra(DevelopersDataKey.DeveloperName)
+            val developerDescription = intent.getStringExtra(DevelopersDataKey.DeveloperDescription)
+
+            val developerLogo = intent.getStringExtra(DevelopersDataKey.DeveloperLogo)
+            val developerCoverImage = intent.getStringExtra(DevelopersDataKey.DeveloperCoverImage)
+
+            val developerCountry = intent.getStringExtra(DevelopersDataKey.DeveloperCountry)
+            val developerCountryFlag = intent.getStringExtra(DevelopersDataKey.DeveloperCountryFlag)
+
+            val developerEmail = intent.getStringExtra(DevelopersDataKey.DeveloperEmail)
+            val developerWebsite = intent.getStringExtra(DevelopersDataKey.DeveloperWebsite)
+
+            val developerSocialMedia = intent.getStringExtra(DevelopersDataKey.DeveloperSocialMedia)
+            val developerSocialMediaLink = intent.getStringExtra(DevelopersDataKey.DeveloperSocialMediaLink)
+
+            val productsApplicationsId = if (intent.hasExtra(DevelopersDataKey.DeveloperApplications)) { intent.getStringExtra(DevelopersDataKey.DeveloperApplications) } else { null }
+            val productsGamesId = if (intent.hasExtra(DevelopersDataKey.DeveloperGames)) { intent.getStringExtra(DevelopersDataKey.DeveloperGames) } else { null }
+            val productsBooksId = if (intent.hasExtra(DevelopersDataKey.DeveloperBooks)) { intent.getStringExtra(DevelopersDataKey.DeveloperBooks) } else { null }
+            val developerMoviesId = if (intent.hasExtra(DevelopersDataKey.DeveloperMovies)) { intent.getStringExtra(DevelopersDataKey.DeveloperMovies) } else { null }
+
+
+
+        } else {
+
+            this@DeveloperIntroductionPage.finish()
+
+        }
 
     }
 
