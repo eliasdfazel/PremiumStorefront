b8f0a686a30e9339a673cd65d4b75c42857935fe
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-07-20 09:02:16 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/Utils/UI/Views/NextedScrollView/NextedScrollView.kt
+++ PremiumStorefrontPhone/src/main/java/co/geeksempire/premium/storefront/Utils/UI/Views/NextedScrollView/NextedScrollView.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 7/19/21, 12:52 PM
+ * Last modified 7/20/21, 8:58 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -13,6 +13,7 @@ package co.geeksempire.premium.storefront.Utils.UI.Views.NextedScrollView
 import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
+import android.util.Log
 import androidx.core.widget.NestedScrollView
 import co.geeksempire.premium.storefront.R
 
@@ -21,11 +22,12 @@ class NextedScrollView(context: Context, attributesSet: AttributeSet) : NestedSc
 
     var typedArray: TypedArray = context.obtainStyledAttributes(attributesSet, R.styleable.NextedScrollView)
 
-    val flingVelocityFraction: Int = typedArray.getInt(R.styleable.NextedScrollView_flingVelocityFraction, 3)
+    val flingVelocityFraction: Int = typedArray.getInt(R.styleable.NextedScrollView_flingVelocityFraction, 5)
 
     override fun fling(velocityY: Int) {
         super.fling(velocityY / flingVelocityFraction)
 
+        Log.d(this@NextedScrollView.javaClass.simpleName, "Velocity Fraction: ${flingVelocityFraction}")
     }
 
 }
