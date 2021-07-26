2c4840eb93b5243ed1fe13aef702682e1ca20109
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-07-26 06:46:50 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ PremiumStorefrontPhoneMovies/src/main/java/co/geeksempire/premium/storefront/movies/EntryConfigurationsMovies.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 7/26/21, 6:46 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.premium.storefront.movies
+
+import co.geeksempire.premium.storefront.StorefrontConfigurations.DataStructure.ProductDataKey
+import co.geeksempire.premium.storefront.StorefrontConfigurations.StorefrontActivity
+import co.geeksempire.premium.storefront.Utils.Data.openPlayStoreToInstall
+import com.google.firebase.inappmessaging.model.Action
+import com.google.firebase.inappmessaging.model.InAppMessage
+import java.util.*
+
+class EntryConfigurationsMovies : StorefrontActivity() {
+
+    override fun networkAvailable() {
+
+
+    }
+
+    override fun networkLost() {
+
+
+    }
+
+    override fun messageClicked(inAppMessage: InAppMessage, action: Action) {
+
+        val actionUrl = action.actionUrl
+
+        val dataMessage: HashMap<String, String> = inAppMessage.data as HashMap<String, String>
+
+        if (dataMessage[ProductDataKey.ProductPackageName] != null &&
+            dataMessage[ProductDataKey.ProductName] != null &&
+            dataMessage[ProductDataKey.ProductSummary] != null) {
+
+            val applicationPackageName = dataMessage[ProductDataKey.ProductPackageName]!!
+            val applicationName = dataMessage[ProductDataKey.ProductName]!!
+            val applicationSummary = dataMessage[ProductDataKey.ProductSummary]!!
+
+            openPlayStoreToInstall(context = applicationContext,
+                aPackageName = applicationPackageName,
+                applicationName = applicationName,
+                applicationSummary = applicationSummary)
+
+        }
+
+    }
+}
