d22643bc82a9299e78ae4e0f29c0acc93a8ef74c
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-07-30 11:05:56 -0800

    [Description] #
    [PremiumStorefront][Applications][ChangeLog]
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ GeeksEmpireLayoutManager/src/main/java/co/geeksempire/geeksempire/layoutmanager/Curve/SimpleAnimatorListener.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 7/30/21, 11:04 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.geeksempire.layoutmanager.Curve;
+
+import android.animation.Animator;
+
+/**
+ * Simple implementation of {@link Animator.AnimatorListener} interface
+ *
+ * @author alex yarovoi
+ * @version 1.0
+ */
+class SimpleAnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+}
---
+++ GeeksEmpireLayoutManager/src/main/java/co/geeksempire/geeksempire/layoutmanager/Curve/ViewAnimationInfo.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 7/30/21, 11:04 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.geeksempire.layoutmanager.Curve;
+
+import android.view.View;
+
+/**
+ * Holder for data
+ *
+ * @author alex yarovoi
+ * @version 1.0
+ */
+class ViewAnimationInfo {
+    int startLeft;
+    int finishLeft;
+    int startRight;
+    int finishRight;
+    int top;
+    int bottom;
+    View view;
+}
