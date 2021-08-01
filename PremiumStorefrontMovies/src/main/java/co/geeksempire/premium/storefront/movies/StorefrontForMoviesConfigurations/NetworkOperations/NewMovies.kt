ed56c6023764b49e627fedbfc42a16b182c76d65
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-08-01 09:49:00 -0800

    [Description] #
    [PremiumStorefront][Applications][ChangeLog]
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ PremiumStorefrontMovies/src/main/java/co/geeksempire/premium/storefront/movies/StorefrontForMoviesConfigurations/NetworkOperations/NewMovies.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 8/1/21, 9:48 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.premium.storefront.movies.StorefrontForMoviesConfigurations.NetworkOperations
+
+class NewMovies {
+}
