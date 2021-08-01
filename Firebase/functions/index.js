263aa1f6680836baf1d435a1dca42888c8a62362
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-08-01 10:51:54 -0800

    [Description] #
    [PremiumStorefront][Applications][ChangeLog]
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -755,3 +755,155 @@ async function setProductsGamesData(jsonObject) {
     /* End - Attributes */
 
 }
+
+exports.transferMoviesData = functions.runWith(runtimeOptions).https.onRequest((req, res) => {
+
+    var numberOfPage = req.query.numberOfPage;
+
+    if (numberOfPage == null) {
+        numberOfPage = 1;
+    }
+
+    var applicationsEndpoint = 'https://geeksempire.co/wp-json/wc/v3/products?consumer_key=ck_e469d717bd778da4fb9ec24881ee589d9b202662&consumer_secret=cs_ac53c1b36d1a85e36a362855d83af93f0d377686'
+        + '&page=' + numberOfPage
+        + '&per_page=100'
+        + '&category=982'
+        + '&orderby=date'
+        + '&order=asc'; 
+
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open('GET', applicationsEndpoint, true);
+    xmlHttpRequest.setRequestHeader('accept', 'application/json');
+    xmlHttpRequest.setRequestHeader('Content-Type', 'application/json');
+    xmlHttpRequest.onreadystatechange = function () {
+        if (this.readyState == 4) {
+
+        } else {
+
+        }
+    };
+    xmlHttpRequest.onprogress = function () {
+
+    };
+    xmlHttpRequest.onload = function () {
+
+        var jsonArrayParserResponse = JSON.parse(xmlHttpRequest.responseText);
+
+        jsonArrayParserResponse.forEach((jsonObject) => {
+
+            setProductsMoviesData(jsonObject);
+
+        });
+
+    };
+    xmlHttpRequest.send();
+
+});
+
+async function setProductsMoviesData(jsonObject) {
+
+    const IdKey = "id";
+
+    const NameKey = "name";
+    const DescriptionKey = "description";
+    const SummaryKey = "short_description";
+
+    const RegularPriceKey = "regular_price";
+    const SalePriceKey = "sale_price";
+
+    const CategoriesKey = "categories";
+    const TagsKey = "tags";
+
+    const ImagesKey = "images";
+    const ImageKey = "image";
+    const ImageSourceKey = "src";
+
+    const AttributesKey = "attributes";
+    const AttributeOptionsKey = "options";
+
+    var productId = jsonObject[IdKey];
+
+    var uniqueRecommendation = false;
+
+    var applicationName = jsonObject[NameKey];
+
+    var productDescription = jsonObject[DescriptionKey];
+    var productSummary = jsonObject[SummaryKey];
+
+    /* Start - Images */
+    var featuredContentImages = jsonObject[ImagesKey];
+
+    var productPoster = (featuredContentImages[0])[ImageSourceKey];
+    /* End - Images */
+
+    /* Start - Primary Category */
+    var productCategories = jsonObject[CategoriesKey];
+
+    var productCategoryName = "All";
+    var productCategoryId = 15;
+
+    productCategories.forEach((productCategory) => {
+
+        var textCheckpoint = (productCategory)[NameKey].split(" ")[0];
+
+        if (textCheckpoint != "All" && textCheckpoint != "Quick" && textCheckpoint != "Unique") {
+
+            productCategoryName = productCategory[NameKey].split(" ")[0];
+            productCategoryId = productCategory[IdKey];
+
+        }
+
+        if (textCheckpoint == "Unique") {
+
+            uniqueRecommendation = true;
+
+        }
+
+    });
+    /* End - Primary Category */
+
+    /* Start - Document * With Even Directory */
+    var firestoreDirectory = '/' + 'PremiumStorefront'
+        + '/' + 'Products'
+        + '/' + 'Multimedia'
+        + '/' + 'Movies'
+        + '/' + productCategoryName
+        + '/' + productId;
+
+    await firestore.doc(firestoreDirectory).set({
+        productId: productId,
+        productName: applicationName,
+        productDescription: productDescription,
+        productSummary: productSummary,
+        productPoster: productPoster,
+        uniqueRecommendation: uniqueRecommendation,
+    }).then(result => {
+
+
+    }).catch(error => {
+        console.log(error);
+    });
+    /* End - Document * With Even Directory */
+
+    /* Start - Attributes */
+    var contentAttributes = jsonObject[AttributesKey];
+
+    contentAttributes.forEach((attributesJsonObject) => {
+
+        var attributeName = attributesJsonObject[NameKey];
+        var attributeValue = attributesJsonObject[AttributeOptionsKey][0];
+
+        var contentAttribute = {};
+        contentAttribute[attributeName] = attributeValue.toString();
+
+        firestore.doc(firestoreDirectory)
+            .update(contentAttribute).then(result => {
+
+            }).catch(error => {
+                console.log(error);
+            });
+
+    });
+    /* End - Attributes */
+
+}
