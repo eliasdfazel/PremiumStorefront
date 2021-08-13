617d12f54fbe50d503f6bca5738bf93d07636b84
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-08-13 06:38:21 -0800

    [Description] #
    [PremiumStorefront][Applications][ChangeLog]
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ PremiumStorefrontMovies/src/main/java/co/geeksempire/premium/storefront/movies/GenreDetailsConfigurations/UserInterface/GenreDetails.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2021 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 8/13/21, 6:06 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package co.geeksempire.premium.storefront.movies.GenreDetailsConfigurations.UserInterface
+
+class GenreDetails {
+}
