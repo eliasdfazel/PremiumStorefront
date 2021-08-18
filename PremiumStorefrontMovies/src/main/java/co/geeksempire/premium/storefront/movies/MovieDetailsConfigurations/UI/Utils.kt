0cc58e4064f66372ea0ad199576e1d1f336921ee
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-08-18 11:20:07 -0800

    [Description] #
    [PremiumStorefront][Applications][ChangeLog]
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontMovies/src/main/java/co/geeksempire/premium/storefront/movies/MovieDetailsConfigurations/UI/Utils.kt
+++ PremiumStorefrontMovies/src/main/java/co/geeksempire/premium/storefront/movies/MovieDetailsConfigurations/UI/Utils.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 8/18/21, 8:04 AM
+ * Last modified 8/18/21, 11:19 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -23,72 +23,81 @@ import co.geeksempire.premium.storefront.movies.MovieDetailsConfigurations.UserI
 import co.geeksempire.premium.storefront.movies.R
 import co.geeksempire.premium.storefront.movies.Utils.UI.Drawable.applyClearEffectRectangle
 
-fun MovieDetailsPagerAdapter.designOptionsMoviesBackground(moviesDetailsItemBinding: MovieDetailsViewHolder, themeType: Boolean) : LayerDrawable {
+fun MovieDetailsPagerAdapter.designOptionsMoviesBackground(moviesDetailsItemBinding: MovieDetailsViewHolder, themeType: Boolean) : LayerDrawable? {
 
-    var optionsBackground = when (themeType) {
-        ThemeType.ThemeLight -> {
+    return try {
 
-            AppCompatResources.getDrawable(context, R.drawable.movie_details_rating_glowing_frame_light) as LayerDrawable
+        var optionsBackground = when (themeType) {
+            ThemeType.ThemeLight -> {
 
+                AppCompatResources.getDrawable(context, co.geeksempire.premium.storefront.movies.R.drawable.movie_details_rating_glowing_frame_light) as LayerDrawable
+
+            }
+            ThemeType.ThemeDark -> {
+
+                AppCompatResources.getDrawable(context, co.geeksempire.premium.storefront.movies.R.drawable.movie_details_rating_glowing_frame_dark) as LayerDrawable
+
+            }
+            else -> AppCompatResources.getDrawable(context, co.geeksempire.premium.storefront.movies.R.drawable.movie_details_rating_glowing_frame_light) as LayerDrawable
         }
-        ThemeType.ThemeDark -> {
 
-            AppCompatResources.getDrawable(context, R.drawable.movie_details_rating_glowing_frame_dark) as LayerDrawable
+        optionsBackground = applyClearEffectRectangle(negativeSpaceDrawable = optionsBackground, negativeSpaceLayerId = R.id.clearLayer,
+            topLeftCorner = 57,
+            topRightCorner = 57,
+            bottomLeftCorner = 57,
+            bottomRightCorner = 57)
 
-        }
-        else -> AppCompatResources.getDrawable(context, R.drawable.movie_details_rating_glowing_frame_light) as LayerDrawable
+        moviesDetailsItemBinding.productRatingStarsView.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.TRANSPARENT
+            isAntiAlias = true
+
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        })
+        moviesDetailsItemBinding.productRatingStarsView.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+
+        moviesDetailsItemBinding.productContentRatingView.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.TRANSPARENT
+            isAntiAlias = true
+
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        })
+        moviesDetailsItemBinding.productContentRatingView.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+
+        moviesDetailsItemBinding.movieGenreFirst.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.TRANSPARENT
+            isAntiAlias = true
+
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        })
+        moviesDetailsItemBinding.movieGenreFirst.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+
+        moviesDetailsItemBinding.movieGenreSecond.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.TRANSPARENT
+            isAntiAlias = true
+
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        })
+        moviesDetailsItemBinding.movieGenreSecond.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+
+        moviesDetailsItemBinding.movieGenreThird.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.TRANSPARENT
+            isAntiAlias = true
+
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        })
+        moviesDetailsItemBinding.movieGenreThird.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
+
+        optionsBackground
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+
+        null
+
     }
-
-    optionsBackground = applyClearEffectRectangle(negativeSpaceDrawable = optionsBackground, negativeSpaceLayerId = R.id.clearLayer,
-        topLeftCorner = 57,
-        topRightCorner = 57,
-        bottomLeftCorner = 57,
-        bottomRightCorner = 57)
-
-    moviesDetailsItemBinding.productRatingStarsView.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
-        style = Paint.Style.FILL
-        color = Color.TRANSPARENT
-        isAntiAlias = true
-
-        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-    })
-    moviesDetailsItemBinding.productRatingStarsView.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
-
-    moviesDetailsItemBinding.productContentRatingView.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
-        style = Paint.Style.FILL
-        color = Color.TRANSPARENT
-        isAntiAlias = true
-
-        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-    })
-    moviesDetailsItemBinding.productContentRatingView.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
-
-    moviesDetailsItemBinding.movieGenreFirst.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
-        style = Paint.Style.FILL
-        color = Color.TRANSPARENT
-        isAntiAlias = true
-
-        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-    })
-    moviesDetailsItemBinding.movieGenreFirst.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
-
-    moviesDetailsItemBinding.movieGenreSecond.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
-        style = Paint.Style.FILL
-        color = Color.TRANSPARENT
-        isAntiAlias = true
-
-        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-    })
-    moviesDetailsItemBinding.movieGenreSecond.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
-
-    moviesDetailsItemBinding.movieGenreThird.setLayerType(AppCompatButton.LAYER_TYPE_SOFTWARE, Paint().apply {
-        style = Paint.Style.FILL
-        color = Color.TRANSPARENT
-        isAntiAlias = true
-
-        xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-    })
-    moviesDetailsItemBinding.movieGenreThird.setLayerType(AppCompatButton.LAYER_TYPE_HARDWARE, null)
-
-    return optionsBackground
 }
