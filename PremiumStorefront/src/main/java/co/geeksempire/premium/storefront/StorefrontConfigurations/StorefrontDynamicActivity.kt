63518a8d1042b2cd69fc1ce8c227f91aa9cc4880
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-08-20 13:56:21 -0800

    [Description] #
    [PremiumStorefront][Applications][ChangeLog]
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefront/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/StorefrontDynamicActivity.kt
+++ PremiumStorefront/src/main/java/co/geeksempire/premium/storefront/StorefrontConfigurations/StorefrontDynamicActivity.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 8/20/21, 4:50 AM
+ * Last modified 8/20/21, 1:56 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -10,11 +10,13 @@
 
 package co.geeksempire.premium.storefront.StorefrontConfigurations
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import co.geeksempire.premium.storefront.AccountManager.SignInProcess.SignInInterface
 import co.geeksempire.premium.storefront.Utils.NetworkConnections.NetworkConnectionListenerInterface
 import co.geeksempire.premium.storefront.Utils.UI.Gesture.GestureListenerInterface
 import co.geeksempire.premium.storefront.Utils.UI.Views.Fragment.FragmentInterface
+import com.google.android.play.core.splitcompat.SplitCompat
 import com.google.firebase.inappmessaging.FirebaseInAppMessagingClickListener
 
 abstract class StorefrontDynamicActivity : AppCompatActivity(),
@@ -30,6 +32,12 @@ abstract class StorefrontDynamicActivity : AppCompatActivity(),
 
     object BooksModule {
         const val EntryConfigurations = "co.geeksempire.premium.storefront.books.EntryConfigurationsBooks"
+    }
+
+    override fun attachBaseContext(baseContext: Context) {
+        super.attachBaseContext(baseContext)
+
+        SplitCompat.install(this)
     }
 
 }
