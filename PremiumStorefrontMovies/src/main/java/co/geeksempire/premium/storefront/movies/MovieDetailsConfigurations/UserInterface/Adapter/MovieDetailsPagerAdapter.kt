ea2062a45fb2622e09190630710be2a6adab923b
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-08-22 07:38:10 -0800

    [Description] #
    [PremiumStorefront][Applications][ChangeLog]
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefrontMovies/src/main/java/co/geeksempire/premium/storefront/movies/MovieDetailsConfigurations/UserInterface/Adapter/MovieDetailsPagerAdapter.kt
+++ PremiumStorefrontMovies/src/main/java/co/geeksempire/premium/storefront/movies/MovieDetailsConfigurations/UserInterface/Adapter/MovieDetailsPagerAdapter.kt
@@ -2,7 +2,7 @@
  * Copyright © 2021 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 8/22/21, 5:28 AM
+ * Last modified 8/22/21, 7:37 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -496,7 +496,7 @@ class MovieDetailsPagerAdapter (var context: MoviesDetails, var themeType: Boole
 
     private fun generateMovieStarImage(starName: String) : String {
 
-        return "http://geeksempire.co/Assets/PremiumStorefront/Movies/Stars/" + starName.replace(" ", "")
+        return "https://geeksempire.co/Assets/PremiumStorefront/Movies/Stars/" + starName.replace(" ", "")
     }
 
 }
