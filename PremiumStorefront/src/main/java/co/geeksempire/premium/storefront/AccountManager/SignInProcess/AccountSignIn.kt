6710b5e21303645c9b5309d65452a9b0df2dce5b
Elias Fazel <elias.fazel@gmail.com>
2024-06-06 15:46:06 -0400

    [Description]
    [PremiumStorefront][Applications/Games][ChangeLog]
    ~
    
    [PremiumStorefront][Movies][ChangeLog]
    
    [PremiumStorefront][Books][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

--- PremiumStorefront/src/main/java/co/geeksempire/premium/storefront/AccountManager/SignInProcess/AccountSignIn.kt
+++ PremiumStorefront/src/main/java/co/geeksempire/premium/storefront/AccountManager/SignInProcess/AccountSignIn.kt
@@ -13,11 +13,9 @@ package co.geeksempire.premium.storefront.AccountManager.SignInProcess
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.annotation.Keep
 import androidx.appcompat.app.AppCompatActivity
-import co.geeksempire.premium.storefront.Utils.Data.generatePassword
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
@@ -26,15 +24,6 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.async
-import org.json.JSONObject
-import java.io.DataOutputStream
-import java.net.URL
-import java.nio.charset.StandardCharsets
-import javax.net.ssl.HttpsURLConnection
 
 @Keep
 data class AccountData(var usernameId: String, var userEmailAddress: String, var userPassword: String)
@@ -85,8 +74,6 @@ class AccountSignIn (val context: AppCompatActivity, val signInInterface: SignIn
 
                                 signInInterface.signInProcessSucceed(it)
 
-                                createGeeksEmpireUser(currentUser)
-
                             }
 
                         }.addOnFailureListener {
@@ -113,53 +100,6 @@ class AccountSignIn (val context: AppCompatActivity, val signInInterface: SignIn
             }
 
         }
-    }
-
-    private fun createGeeksEmpireUser(firebaseUser: FirebaseUser) = CoroutineScope(SupervisorJob() + Dispatchers.IO).async {
-
-        val endpointAddress = "https://geeksempire.co/wp-json/wp/v2/users/register"
-
-        val serverUrl = URL(endpointAddress)
-
-        val connection = serverUrl.openConnection() as HttpsURLConnection
-        connection.requestMethod = "POST"
-        connection.connectTimeout = 3000
-        connection.doOutput = true
-
-        val usernameId = firebaseUser.email!!.split("@").first()
-        val userEmailAddress = firebaseUser.email!!
-        val userPassword = generatePassword(firebaseUser.uid)
-
-        val inputData = JSONObject("{" +
-                "\"username\": \"${usernameId}\"," +
-                "\"email\": \"${userEmailAddress}\"," +
-                "\"password\": \"${userPassword}\"" +
-                "}").toString()
-
-        val postData: ByteArray = inputData.toByteArray(StandardCharsets.UTF_8)
-
-        connection.setRequestProperty("charset", "utf-8")
-        connection.setRequestProperty("Content-length", postData.size.toString())
-        connection.setRequestProperty("Content-Type", "application/json")
-
-        try {
-
-            val outputStream: DataOutputStream = DataOutputStream(connection.outputStream)
-
-            outputStream.write(postData)
-            outputStream.flush()
-
-        } catch (exception: Exception) {
-            exception.printStackTrace()
-        }
-
-        if (connection.responseCode == HttpsURLConnection.HTTP_OK) {
-            Log.d(this@AccountSignIn.javaClass.simpleName, "Geeks Empire User Created")
-
-            signInInterface.userCreated(AccountData(usernameId, userEmailAddress, userPassword))
-
-        }
-
     }
 
 }
